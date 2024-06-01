5f15475f617813f57b21f9b5a3104d7286b5c34d
powerrand <powerrand4@gmail.com>
2024-06-02 00:01:53 +0800

    classname的更改

--- src/Header.js
+++ src/Header.js
@@ -7,58 +7,67 @@ import { useState } from "react";
 function Header() {
     const [SliderTransform, setSliderTransform] = useState(0);
 
+    function getSliderElement() {
+        return document.getElementById('slider')
+    }
+
+    function moveSlider(number) {
+        const slider = getSliderElement()
+        slider.style.transform = `translateX(calc(var(--menu-width) * ${number}))`
+       /* console.log(parseFloat(window.getComputedStyle(slider).transform.match(/matrix\((.+)\)/)[1].split(', ')[4]))
+        console.log(window.getComputedStyle(slider).transform.match(/matrix\((.+)\)/)[1].split(', '))
+        console.log(window.getComputedStyle(slider).transform)*/
+    }
+
     function HandleClick(number) {
-        let slider = document.getElementById('slider');
-        slider.style.transform= "translateX(calc(var(--menu-background-width) * (" + number +")))";
-        setSliderTransform(number);
+        moveSlider(number)
+        setSliderTransform(number)
     };
     function HandleMouseEnter (number){
-        let slider = document.getElementById('slider');
-        slider.style.transform= "translateX(calc(var(--menu-background-width) * (" + number +")))";
+        moveSlider(number)
 
     };
     function HandleMouseLeave(){
-        let slider = document.getElementById('slider');
-        slider.style.transform="translateX(calc(var(--menu-background-width) * (" + SliderTransform +")))";
+        moveSlider(SliderTransform)
     };
     
   return (
-    <div className="avigationBar-menu-container">
-        <div className="header_logo-container"> 
+    <div className="nav-menu-container">
+        <div className="logo-container"> 
              <Link to="/side-project" >
-            <img className="header_logo_png" src={Logo} alt="logo"></img> 
+            <img className="logo" src={Logo} alt="logo"></img> 
             </Link>
         </div>
-        <div className="navigation-menu-top-layer-container">
-            <div className="navigation-menu-top-layer-container-backgroundColor">
-                <ul className="navigation-menu-top-layer">
-                    <li className="navigation-menu-item" onMouseEnter={() => HandleMouseEnter("0")} onMouseLeave={HandleMouseLeave}>
+        <div className="menu-layer-container">
+            <div className="menu-layer-bg">
+                <ul className="menu-layer">
+                    <li className="menu-item" onMouseEnter={() => HandleMouseEnter("0")} onMouseLeave={HandleMouseLeave}>
                         <Link to="/side-project" >
-                        <p onClick={() => HandleClick("0")} className="navigation-menu-item-label"> 吊飾</p>
+                        <p onClick={() => HandleClick("0")} className="menu-item-label"> 吊飾</p>
                         </Link>
                     </li>
-                    <li className="navigation-menu-item" onMouseEnter={() => HandleMouseEnter("1")} onMouseLeave={HandleMouseLeave}>
+                    <li className="menu-item" onMouseEnter={() => HandleMouseEnter("1")} onMouseLeave={HandleMouseLeave}>
                         <Link to="/side-project">
-                        <p onClick={() => HandleClick("1")} className="navigation-menu-item-label">貼紙</p>
+                        <p onClick={() => HandleClick("1")} className="menu-item-label">貼紙</p>
                         </Link>
                     </li>
-                    <li className="navigation-menu-item" onMouseEnter={() => HandleMouseEnter("2")} onMouseLeave={HandleMouseLeave}>
+                    <li className="menu-item" onMouseEnter={() => HandleMouseEnter("2")} onMouseLeave={HandleMouseLeave}>
                         <Link to="/side-project">
-                        <p onClick={() => HandleClick("2")} className="navigation-menu-item-label">玩偶</p>
+                        <p onClick={() => HandleClick("2")} className="menu-item-label">玩偶</p>
                         </Link>
                     </li>
-                    <li className="navigation-menu-item" onMouseEnter={() => HandleMouseEnter("3")} onMouseLeave={HandleMouseLeave}>
+                    <li className="menu-item" onMouseEnter={() => HandleMouseEnter("3")} onMouseLeave={HandleMouseLeave}>
                         <Link to="/side-project">
-                        <p onClick={() => HandleClick("3")} className="navigation-menu-item-label">燈飾</p>
+                        <p onClick={() => HandleClick("3")} className="menu-item-label">燈飾</p>
                         </Link>
                     </li>
                 </ul>
                 <div id="slider"></div>
             </div>
         </div>
-        <div className="header_login-container">
+        <div className="login-container">
             <Link to="/sing">
-            <img className="header_logo_png" src={User} alt="user"></img>
+            <img className="user-icon" src={User} alt="user"></img>
             </Link>
         </div>
     </div>
