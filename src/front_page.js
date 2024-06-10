f0107255c16e6a8e9e3cd790ca8bbcf84cdc0c88
powerrand <powerrand4@gmail.com>
2024-06-11 05:14:01 +0800

    更新主頁畫面，並修改選單樣式，和把圖片跟連結元素變成無法拖動

--- src/front_page.js
+++ src/front_page.js
@@ -1,9 +1,24 @@
-
+import pendant1 from "./picture/pendant1.jpg";
 function FrontPage() {
 return(
     <div className="CustomPage">
         <div className="Grid-row-wrapper">
-            <p>測試</p>
+            <div className="Grid-row-wrapper-div">
+                <img className="Grid-row-wrapper-img" src={pendant1} alt=""></img>
+            </div>
+            <div className="Grid-row-wrapper-div">
+                <img className="Grid-row-wrapper-img" src={pendant1} alt=""></img>
+            </div>
+            <div className="Grid-row-wrapper-div">
+                <img className="Grid-row-wrapper-img" src={pendant1} alt=""></img>
+            </div>
+            <div className="Grid-row-wrapper-div">
+                <img className="Grid-row-wrapper-img" src={pendant1} alt=""></img>
+            </div>
+            <div className="Grid-row-wrapper-div">
+                <img className="Grid-row-wrapper-img" src={pendant1} alt=""></img>
+            </div>
+            {/* <a href="/">我出門買吃的，沒帶手機，等等回來</a> */}
         </div>
     </div>
     )
