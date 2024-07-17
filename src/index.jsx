d9eaeb6e0dbb8f916e3b9ac598a0483059331589
powerrand <powerrand4@gmail.com>
2024-07-18 07:44:53 +0800

    將Css檔案做分離

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./CSS/Header.css";
 import "./CSS/front_page.css";
+import "./CSS/Marquee.css"
 import "./CSS/HorizontalScroll.css";
 import "./CSS/CursorFollow.css";
 import "./Test/Css/Test.css";
